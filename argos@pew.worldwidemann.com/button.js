1e6e2d5e469eaf38480cdd51477bea3af3ccdc63
Martin Wilck <mwilck@suse.com>
2020-04-16 00:29:47 +0200

    button.js: eliminate undefined property warning
    JS WARNING: [utilities.js 135]: reference to undefined property "alternate"

--- argos@pew.worldwidemann.com/button.js
+++ argos@pew.worldwidemann.com/button.js
@@ -191,8 +191,10 @@ var ArgosButton = new Lang.Class({
         menuItem.actor.insert_child_below(lineView, menuItem.label);
         menuItem.label.visible = false;
         menus[dropdownLines[i + 1].menuLevel] = menuItem.menu;
-      } else if ((i + 1) < dropdownLines.length && dropdownLines[i + 1].menuLevel === dropdownLines[i].menuLevel &&
-        dropdownLines[i + 1].alternate === "true") {
+      } else if ((i + 1) < dropdownLines.length &&
+		 dropdownLines[i + 1].menuLevel === dropdownLines[i].menuLevel &&
+		 dropdownLines[i + 1].hasOwnProperty("alternate") &&
+		 dropdownLines[i + 1].alternate === "true") {
         menuItem = new ArgosMenuItem(this, dropdownLines[i], dropdownLines[i + 1]);
         // Skip alternate line
         i++;
